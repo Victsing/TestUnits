2285ea82408a563e0042ff5c4169fac948e92e0b
leandredb75 <leandredibipro@gmail.com>
2021-12-29 23:14:34 +0100

    clean 2

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -3,63 +3,76 @@ const User = db.users;
 const Op = db.Sequelize.Op;
 
 exports.create = async (req, res) => {
-  const {data} = req.body
+  const {
+    firstname,
+    lastname,
+    email,
+    password,
+    age,
+  } = req.body
+
   try {
-    const user = await User.create({data})
-    res.json({status: true, user})
+    const user = await User.create({
+      firstname,
+      lastname,
+      email,
+      password,
+      age,
+    })
+    res.json({ status: true, user })
   } catch (e) {
-    res.json({status: false, e})
+    res.json({ status: false, e })
   }
 };
 
 exports.findAll = async (req, res) => {
   try {
     const user = await User.findAll();
-    res.json({status: true, user})
+    res.json({ status: true, user })
   } catch (e) {
-    res.json({status: false, e})
+    res.json({ status: false, e })
   }
 };
 
 exports.findOne = async (req, res) => {
-  const {id} = req.body
+  const { id } = req.body
   try {
     const user = await User.findOne({
       where: {
         id
       }
     })
-    res.json({status: true, user})
+    res.json({ status: true, user })
   } catch (e) {
-    res.json({status: false, e})
+    res.json({ status: false, e })
   }
 };
 
 exports.update = async (req, res) => {
-  const {data} = req.body
+  const { data } = req.body
   try {
-    const user = await User.update({data}, {
+    const user = await User.update({ data }, {
       where: {
         id
       }
     })
-    res.json({status: true, user})
+    res.json({ status: true, user })
   } catch (e) {
-    res.json({status: false, e})
+    res.json({ status: false, e })
   }
 };
 
 exports.delete = async (req, res) => {
-  const {id} = req.body
+  const { id } = req.body
   try {
     const user = await User.detele({
       where: {
         id
       }
     })
-    res.json({status: true, user})
+    res.json({ status: true, user })
   } catch (e) {
-    res.json({status: false, e})
+    res.json({ status: false, e })
   }
 };
 
---
+++ app/users.js
@@ -0,0 +1,31 @@
+module.exports = users => {
+    constructor(email, lastName, firstName, dateString) {
+    this.email = email
+    this.lastName = lastName
+    this.firstName = firstName
+    this.dateString = dateString
+    this.emailCheck = new EmailService()
+    }
+
+
+    checkMyEmail() {
+    this.emailCheck.checkEmailService()
+    }
+
+    UserEmail() {
+    return this.email
+    }
+
+    UserLastName() {
+    return this.lastName
+    }
+
+    UserFirstName() {
+    return this.firstName
+    }
+
+    UserBornDate() {
+    const birthday = +new Date(this.dateString);
+    return ((Date.now() - birthday) / (31557600000));
+    }
+};
