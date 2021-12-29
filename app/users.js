e09705028a642d77602b78a5aee0b9086fdb6543
leandredb75 <leandredibipro@gmail.com>
2021-12-29 23:16:01 +0100

    clean 2

--- app/users.js
+++ app/users.js
@@ -1,31 +1,33 @@
 module.exports = users => {
-    constructor(email, lastName, firstName, dateString) {
-    this.email = email
-    this.lastName = lastName
-    this.firstName = firstName
-    this.dateString = dateString
-    this.emailCheck = new EmailService()
-    }
+    class Users {
+        constructor(email, lastName, firstName, dateString) {
+            this.email = email
+            this.lastName = lastName
+            this.firstName = firstName
+            this.dateString = dateString
+            this.emailCheck = new EmailService()
+        }
 
 
-    checkMyEmail() {
-    this.emailCheck.checkEmailService()
-    }
+        checkMyEmail() {
+            this.emailCheck.checkEmailService()
+        }
 
-    UserEmail() {
-    return this.email
-    }
+        UserEmail() {
+            return this.email
+        }
 
-    UserLastName() {
-    return this.lastName
-    }
+        UserLastName() {
+            return this.lastName
+        }
 
-    UserFirstName() {
-    return this.firstName
-    }
+        UserFirstName() {
+            return this.firstName
+        }
 
-    UserBornDate() {
-    const birthday = +new Date(this.dateString);
-    return ((Date.now() - birthday) / (31557600000));
+        UserBornDate() {
+            const birthday = +new Date(this.dateString);
+            return ((Date.now() - birthday) / (31557600000));
+        }
     }
 };
